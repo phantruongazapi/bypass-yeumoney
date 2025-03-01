48ef5c9dcc739084a6716fbb440c25630d2015d2
phantruongazapi <trungtruongphan116@gmail.com>
2025-03-01 21:25:25 +0700

    Update freeyeumoney.user.js

--- freeyeumoney.user.js
+++ freeyeumoney.user.js
@@ -4,8 +4,8 @@
 // @version      3.2.7
 // @description  Bypass Yeumoney premium
 // @require      https://github.com/phantruongazapi/bypass-yeumoney/raw/refs/heads/main/free.js
-// @updateURL    https://github.com/phantruongazapi/premium/raw/refs/heads/main/pre.user.js
-// @downloadURL  https://github.com/phantruongazapi/premium/raw/refs/heads/main/pre.user.js
+// @updateURL    https://github.com/phantruongazapi/bypass-yeumoney/raw/refs/heads/main/freeyeumoney.user.js
+// @downloadURL  https://github.com/phantruongazapi/bypass-yeumoney/raw/refs/heads/main/freeyeumoney.user.js
 // @author       script by phantruong
 // @match        https://yeumoney.com/*
 // @grant        GM_xmlhttpRequest
